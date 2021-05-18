2916d5fd174d417afbc5180c4cb0511a651b3699
Mavi <mavitoktamispaker@gmail.com>
2021-05-18 17:19:02 -0400

    all quicksort tests passing

--- app/src/main/java/stacks/queues/java/MyQuickSort.java
+++ app/src/main/java/stacks/queues/java/MyQuickSort.java
@@ -1,4 +1,40 @@
 package stacks.queues.java;
 
 public class MyQuickSort {
+
+    public static int[] quicksort(int[] array) {
+        quicksort(array, 0, array.length - 1);
+        return array;
+    }
+
+    public static void quicksort(int[] array, int left, int right) {
+        if (left >= right) {
+            return;
+        }
+        int pivot = array[(left + right) / 2];
+        int index = partition(array, left, right, pivot);
+        quicksort(array, left, index - 1);
+        quicksort(array, index, right);
+    }
+
+    public static int partition(int[] array, int left, int right, int pivot) {
+        while (left <= right) {
+            while (array[left] < pivot) {
+                left++;
+            }
+
+            while (array[right] > pivot){
+                right--;
+            }
+
+            if (left <= right){
+                array[left] = (array[left] + array[right]) - (array[right] = array[left]);
+
+                left++;
+                right--;
+            }
+        }
+
+        return left;
+    }
 }
--- app/src/test/java/stacks/queues/java/MyQuickSortTest.java
+++ app/src/test/java/stacks/queues/java/MyQuickSortTest.java
@@ -1,4 +1,39 @@
 package stacks.queues.java;
 
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 public class MyQuickSortTest {
+    @Test
+    @DisplayName("can partition on a pivot")
+    public void canPartition(){
+        int[] array = {6,5,4,3};
+        int partition = MyQuickSort.partition(array, 0, 3, 5);
+        assertEquals(partition, 2);
+    }
+
+    @Test
+    @DisplayName("sort already sorted")
+    public void sortAlreadySorted(){
+        int[] array = {3, 4, 5, 6};
+        int[] sorted = MyQuickSort.quicksort(array);
+        assertEquals(sorted[0], 3);
+        assertEquals(sorted[1], 4);
+        assertEquals(sorted[2], 5);
+        assertEquals(sorted[3], 6);
+    }
+
+    @Test
+    @DisplayName("sort from reversed")
+    public void sortFromReversed(){
+        int[] array = {6,5,4,3};
+        int[] sorted = MyQuickSort.quicksort(array);
+        assertEquals(sorted[0], 3);
+        assertEquals(sorted[1], 4);
+        assertEquals(sorted[2], 5);
+        assertEquals(sorted[3], 6);
+    }
 }
